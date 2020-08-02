93c94a8bcedaaebe38ba8a677f18242bb24dd56d
katiewu71 <katiewu71@gmail.com>
2020-08-02 19:12:55 -0400

    Initial Commit

---
+++ CodePath Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodePath Tip Calculator
+//
+//  Created by Katie Wu on 8/2/20.
+//  Copyright © 2020 KatieWu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
